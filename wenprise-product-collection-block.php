f6ad10fb2af836567d0ece6081ab16c3a15ac3d8
Amos Lee <amos@wpcio.com>
2026-08-21 12:19:20 +0800

    Update plugin author name

--- wenprise-product-collection-block.php
+++ wenprise-product-collection-block.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:           The WordPress Contributors
+ * Author:           文普睿思信息科技
  * License:          GPL-2.0-or-later
  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:      wenprise-product-collection
